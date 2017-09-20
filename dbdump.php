cc6ab507f425d7f581bef0ed14dc3a5e7244ca21
ResetButton <counterfate@gmail.com>
2017-09-20 10:58:41 +0300

    Added MODx and readme update

--- dbdump.php
+++ dbdump.php
@@ -1,34 +1,39 @@
 <?php
 set_time_limit (0);
 
-//Check WORDPRESS
+//WORDPRESS
 if (file_exists('wp-config.php')) {
     include('wp-config.php');
-    echo "Detected: WP<br>";
-    MakeDump (DB_HOST,DB_NAME,DB_USER,DB_PASSWORD);
+    MakeDump (DB_HOST,DB_NAME,DB_USER,DB_PASSWORD,"Wordress");
 }
-//Check OPENCART
+//OPENCART
 if (file_exists('config.php')) {
     include('config.php');
-    echo "Detected: Opencart<br>";
-    MakeDump (DB_HOSTNAME,DB_DATABASE,DB_USERNAME,DB_PASSWORD);    
+    MakeDump (DB_HOSTNAME,DB_DATABASE,DB_USERNAME,DB_PASSWORD, "Opencart");
 
 }
 
-//Check Joomla
+//Joomla
 if (file_exists('configuration.php')) {
     include('configuration.php');
-    echo "Detected: Joomla<br>";
     $config=new JConfig;
-    MakeDump ($config->host,$config->db,$config->user,$config->password);    
+    MakeDump ($config->host,$config->db,$config->user,$config->password,"Joomla");
 
 }
 
+//MODx Revolution:
+if (file_exists('core/config/config.inc.php')) {
+    include('core/config/config.inc.php');
+    MakeDump ($database_server,$dbase,$database_user,$database_password,"MODx Revolution");
+}
 
-function MakeDump($dbhost,$dbname,$dbuser,$dbpassword)
+
+function MakeDump($dbhost,$dbname,$dbuser,$dbpassword,$engine)
 {
+    echo "Engine recognized as -> $engine<br>";
     shell_exec('mysqldump -u'.$dbuser.' -h'.$dbhost.' -p'.$dbpassword.' '.$dbname.' > '.$dbname.'.sql');
-    echo "Dump done, "; 
+    echo "Dump done, ";
+
     if (isset($_SERVER['REQUEST_URI'])) {
     $current_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
     echo "Database file -> ".dirname($current_url)."/".$dbname.".sql";
